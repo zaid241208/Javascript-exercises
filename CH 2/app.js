d825cecf44ea19f43ce28d691b1c405369c06b45
zaid <zk03005363611@gmail.com>
2026-03-30 01:40:57 +0500

    Add files via upload

---
+++ CH 2/app.js
@@ -0,0 +1,82 @@
+// -----------------=================||||<<< NEW CHAPTER >>>>||||=================-----------------
+
+
+
+// <!-- ---===||   CHAPTER 2   ||===--- -->
+
+// <!-- ---answer 1--- -->
+
+// let username;
+
+
+
+// <!-- ---answer 2--- -->
+
+// let myname = "Zaid Khan";
+
+
+
+// <!-- ---answer 3--- -->
+
+// let message;
+// message = "Hello";
+// alert(message);
+
+
+
+// <!-- ---answer 4--- -->
+
+// let st_name;
+// let st_age;
+// let st_institute;
+
+// st_name = "john";
+// st_age = 16;
+// st_institute = "Random";
+
+// alert("student name = " + st_name);
+// alert("student age = " + st_age);
+// alert("student institute = " + st_institute);
+
+
+
+// <!-- ---answer 5--- -->
+
+// let example;
+// example = pizza
+
+
+
+// <!-- ---answer 6--- -->
+
+// let email;
+// email = "e.g zaid@gmail.com"
+
+// alert("my Email is = " + email)
+
+
+
+// <!-- ---answer 7--- -->
+// let book;
+// book = "A smarter way to way learn JavaScript"
+
+// alert("i am trying to learn from the book " + book)
+
+
+
+// <!-- ---answer 8--- -->
+
+// document.getElementById("js").innerHTML = 
+// 'Yah! I can write <span style="font-size: large; font-weight: bold;">HTML</span> content through JavaScript';
+
+
+
+// <!-- ---answer 9--- -->
+
+// let emoji;
+
+// emoji = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+
+// alert(emoji);
+
+// document.getElementById('js').innerHTML = emoji;
