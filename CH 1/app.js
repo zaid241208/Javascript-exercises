9ff4aa91effce344d6a2510fe110557ee6ecb813
zaid <zk03005363611@gmail.com>
2026-03-30 01:40:43 +0500

    Add files via upload

---
+++ CH 1/app.js
@@ -0,0 +1,35 @@
+// <!-- ---===||   CHAPTER 1   ||===--- -->
+
+// answer 1
+// alert("Welcome")
+
+// ---------------------------------------
+
+// answer 2
+// alert("Error! Please Enter a valid password")
+
+// ---------------------------------------
+
+// answer 3
+// alert("Welocme to JS Land.... \n Happy Coding")
+
+// ---------------------------------------
+
+// answer 4
+// alert("Welcome to JS Land....")
+// alert("Enjoy Coding")
+
+// ---------------------------------------
+
+// answer 5
+// console.log("Hello, I can run JS through my web browser's console!")
+
+// ---------------------------------------
+
+// answer 6
+// alert("Error! Done")
+
+// ---------------------------------------
+
+// answer 7
+// visit index.html for answer 7
