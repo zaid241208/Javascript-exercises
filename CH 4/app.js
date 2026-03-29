dce9f9397c5aa14711cfdedf1ceb8466accf9b6a
zaid <zk03005363611@gmail.com>
2026-03-30 01:41:27 +0500

    Add files via upload

---
+++ CH 4/app.js
@@ -0,0 +1,33 @@
+// -----------------=================||||<<< NEW CHAPTER >>>>||||=================-----------------
+
+// <!-- ---===||   CHAPTER 4   ||===--- -->
+
+// <!-- ---answer 1 --- -->
+
+// let x, y, z;
+
+// x = zaid;
+// y = tahir;
+// z = maddi;
+
+
+
+// <!-- ---answer 2 --- -->
+
+// let name;
+// let zaid1;
+// let _zaidKhan;
+// let $zaid;
+
+// let 1zaid;
+// let zaid-;
+// let var;
+// let zaid khan;
+// let @zaid;
+
+
+
+// <!-- ---answer 3 --- -->
+
+document.getElementById("js").innerHTML =
+    `<h2>Rules for naming JS variables</h2> Variables names only contain, numbers, $ and _ . For example : $my_1st Varriable <br> Variables must begin with a letter, $ or _ . For example : $name, _name or name <br> Variable names are case sensitive <br> Variable names should not be JS keywords`;
