06c88f807241d11b36d17e72c793658f46bfecba
zaid <zk03005363611@gmail.com>
2026-03-30 01:41:40 +0500

    Add files via upload

---
+++ CH 5/app.js
@@ -0,0 +1,179 @@
+// -----------------=================||||<<< NEW CHAPTER >>>>||||=================-----------------
+
+// <!-- ---===||   CHAPTER 5   ||===--- -->
+
+// <!-- ---answer 1 --- -->
+
+// let value_1;
+// let value_2;
+// let value_3;
+
+// value_1 = +prompt("enter value to add");
+// value_2 = +prompt("enter second value");
+// value_3 = value_1 + value_2;
+
+// document.getElementById("js").innerHTML = 
+// `Sum of ${value_1} and ${value_2} is ${value_3}`
+
+
+
+// <!-- ---answer 2 PART(1) --- -->
+
+// let value_1;
+// let value_2;
+// let value_3;
+
+// value_1 = +prompt("enter value to subtract");
+// value_2 = +prompt("enter second value");
+// value_3 = value_1 - value_2;
+
+// document.getElementById("js").innerHTML = 
+// `subtraction of ${value_1} and ${value_2} is ${value_3}`
+
+
+
+// <!-- ---answer 2 PART(2) --- -->
+
+// let value_1;
+// let value_2;
+// let value_3;
+
+// value_1 = +prompt("enter value to multiply");
+// value_2 = +prompt("enter second value");
+// value_3 = value_1 * value_2;
+
+// document.getElementById("js").innerHTML = 
+// `subtraction of ${value_1} and ${value_2} is ${value_3}`
+
+
+
+// <!-- ---answer 2 PART(3) --- -->
+
+// let value_1;
+// let value_2;
+// let value_3;
+
+// value_1 = +prompt("enter value to multiply");
+// value_2 = +prompt("enter second value");
+// value_3 = value_1 * value_2;
+
+// document.getElementById("js").innerHTML = 
+// `subtraction of ${value_1} and ${value_2} is ${value_3}`
+
+
+
+// <!-- ---answer 2 PART(4) --- -->
+
+// let value_1;
+// let value_2;
+// let value_3;
+
+// value_1 = +prompt("enter value to divide");
+// value_2 = +prompt("enter second value");
+// value_3 = value_1 / value_2;
+
+// document.getElementById("js").innerHTML = 
+// `subtraction of ${value_1} and ${value_2} is ${value_3}`
+
+
+
+// <!-- ---answer 2 PART(5) --- -->
+
+// let value_1;
+// let value_2;
+// let value_3;
+
+// value_1 = +prompt("enter value for modulus");
+// value_2 = +prompt("enter second value");
+// value_3 = value_1 % value_2;
+
+// document.getElementById("js").innerHTML = 
+// `subtraction of ${value_1} and ${value_2} is ${value_3}`
+
+
+
+// <!-- ---answer 3 --- -->
+
+// let num;
+
+// document.getElementById("js").innerHTML =
+//     `Value after variable declaration is: ${num} <br>`;
+
+// num = 5;
+// document.getElementById("js").innerHTML +=
+//     `Initial value: ${num} <br>`;
+
+// num++;
+// document.getElementById("js").innerHTML +=
+//     `Value after increment is: ${num} <br>`;
+
+// num += 7;
+// document.getElementById("js").innerHTML +=
+//     `Value after addition is: ${num} <br>`;
+
+
+// num--;
+// document.getElementById("js").innerHTML +=
+// `Value after decrement is: ${num} <br>`
+
+// let remainder = num % 3;
+// document.getElementById("js").innerHTML +=
+// `The remainder is ${remainder}`
+
+
+
+
+// <!-- ---answer 4 --- -->
+
+// let ticket;
+// let price;
+
+// price = 600;
+// ticket = +prompt("Enter amount of tickets");
+
+// let amount;
+// amount = price * ticket;
+
+// document.getElementById("js").innerHTML =
+// `The cost of buying ${ticket} tickets ${amount}`
+
+
+
+// <!-- ---answer 5 --- -->
+
+let table;
+
+table = prompt("table of = ")
+
+document.getElementById("js").innerHTML =
+`Table of : ${table} <br>`
+
+document.getElementById("js").innerHTML =
+`${table * 1} x 1 = ${table} <br> `
+
+document.getElementById("js").innerHTML +=
+`${table} x 2 = ${table *2} <br>`
+
+document.getElementById("js").innerHTML +=
+`${table} x 3 = ${table *3} <br>`
+
+document.getElementById("js").innerHTML +=
+`${table} x 4 = ${table *4} <br>`
+
+document.getElementById("js").innerHTML +=
+`${table} x 5 = ${table *5} <br>`
+
+document.getElementById("js").innerHTML +=
+`${table} x 6 = ${table *6} <br>`
+
+document.getElementById("js").innerHTML +=
+`${table} x 7 = ${table *7} <br>`
+
+document.getElementById("js").innerHTML +=
+`${table} x 8 = ${table *8} <br>`
+
+document.getElementById("js").innerHTML +=
+`${table} x 9 = ${table *9} <br>`
+
+document.getElementById("js").innerHTML +=
+`${table} x 10 = ${table *10} <br>`
