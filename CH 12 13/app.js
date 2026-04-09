46af3039e818d18310f04f97fe352c12f0b7ae4f
zaid <zk03005363611@gmail.com>
2026-04-09 22:54:26 +0500

    Add files via upload

---
+++ CH 12 13/app.js
@@ -0,0 +1,133 @@
+// -----------------=================||||<<< NEW CHAPTER >>>>||||=================-----------------
+
+// <!-- ---===||   CHAPTER 12-13   ||===--- -->
+
+// <!-- ---answer 1 --- -->
+
+
+// let character;
+
+// character = prompt("Enter Character");
+
+// let code = character.charCodeAt(0);
+
+// if (code >= 48 && code <= 57) {
+//     document.getElementById("js").innerHTML =
+//         `Number hai`
+// } else if (code >= 65 && code <= 90) {
+//     document.getElementById("js").innerHTML =
+//         `Upper case hai`
+// } else if (code >= 97 && code <= 122) {
+//     document.getElementById("js").innerHTML =
+//         `Lower case hai`
+// } else {
+//     document.getElementById("js").innerHTML = 
+//     `Other`
+// }
+
+
+
+// <!-- ---answer 2 --- -->
+
+// let num_1;
+// let num_2;
+
+// num_1 = +prompt("Enter");
+// num_2 = +prompt("Enter");
+
+// if (num_1 > num_2) {
+//     document.getElementById("js").innerHTML =
+//         `${num_1} Barha hai`
+// } else if (num_2 > num_1) {
+//     document.getElementById("js").innerHTML =
+//         `${num_2} Barha hai`
+// } else {
+//     document.getElementById("js").innerHTML =
+//         `Dono barabar hain`
+// }
+
+
+
+// <!-- ---answer 3 --- -->
+
+// let num;
+
+// num = +prompt("Enter number to find");
+
+// if (num < 0) {
+//     alert("given number is negative")
+// } else if (num > 0) {
+//     alert("given number is positive")
+// } else {
+//     alert("number is zero")
+// }
+
+
+
+// <!-- ---answer 4 --- -->
+
+// let char;
+
+// char = prompt("Enter character");
+
+// if(
+//     char === "a" || char === "e" || char === "i" || char === "o" || char === "u" ||
+//     char === "A" || char === "E" || char === "I" || char === "O" || char === "U"
+// ) {
+//     alert("true! its vowel")
+// } else {
+//     alert("false!")
+// }
+
+
+
+// <!-- ---answer 5 --- -->
+
+// let pass;
+// let user_pass;
+
+// pass = "zaid";
+// user_pass = prompt("Enter your password")
+
+// if (user_pass === pass) {
+//     document.getElementById("js").innerHTML =
+//         `Welcome G !`
+// } else {
+//     alert("Please Enter correct password")
+// }
+
+
+
+// <!-- ---answer 6 --- -->
+
+// let greeting;
+
+// let hour;
+
+// hour = 13;
+
+// if (hour < 18) {
+//     alert(greeting = "Good day")
+// } else {
+//     alert(greeting = "Good evening")
+// }
+
+
+
+// <!-- ---answer 7 --- -->
+
+// let time;
+
+// time = +prompt("Enter exact time");
+
+// if (time >= 0 && time < 12) {
+//     alert("Good morning!")
+// } else if (time >= 12 && time < 17) {
+//     alert("Good afternoon!")
+// } else if (time >= 17 && time < 19) {
+//     alert("Good evening!")
+// } else if (time >= 19 && time <= 23) {
+//     alert("Good night!")
+// } else {
+//     alert("Enter correct time")
+// }
