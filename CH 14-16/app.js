417b1f89c432f69daadbb55ecae5bf400007b686
zaid <zk03005363611@gmail.com>
2026-04-10 12:29:50 +0500

    Add files via upload

---
+++ CH 14-16/app.js
@@ -0,0 +1,99 @@
+// -----------------=================||||<<< NEW CHAPTER >>>>||||=================-----------------
+
+// <!-- ---===||   CHAPTER 14-16   ||===--- -->
+
+// <!-- ---answer 1 --- -->
+
+
+// let st_names;
+
+// st_names = [];
+
+
+
+// <!-- ---answer 2 --- -->
+
+// let students;
+
+// students = {};
+
+
+
+// <!-- ---answer 3 --- -->
+
+// let random;
+
+// random = ["kuch bhi"];
+
+
+
+// <!-- ---answer 4 --- -->
+
+let num;
+
+num = [1, 2, 3];
+
+
+
+// <!-- ---answer 5 --- -->
+
+let tf;
+
+tf = [true, false];
+
+
+
+// <!-- ---answer 6 --- -->
+
+let mix;
+
+mix = ["zaid", 20, true];
+
+
+
+// <!-- ---answer 7 --- -->
+
+// let qual;
+
+// qual = ["SSC", "HSC", "BSC", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+// alert(
+//     "1)" + qual[0] + "\n" +
+//     "2)" + qual[1] + "\n" +
+//     "3)" + qual[2] + "\n" +
+//     "4)" + qual[3] + "\n" +
+//     "5)" + qual[4] + "\n" +
+//     "6)" + qual[5] + "\n" +
+//     "7)" + qual[6] + "\n" +
+//     "7)" + qual[7] + "\n"
+// )
+
+
+
+// <!-- ---answer 8 --- -->
+
+// let st;
+// let marks;
+// let total_marks;
+// let result;
+
+// st = ["zaid", "talha", "maaz"];
+
+// marks = [];
+
+// marks[0] = +prompt("Enter");
+// marks[1] = +prompt("Enter");
+// marks[2] = +prompt("Enter");
+
+// total_marks = 500;
+
+// result =
+//     "Score of " + st[0] + " is " + ((marks[0] / total_marks) * 100).toFixed(2) + "%\n" +
+//     "Score of " + st[1] + " is " + ((marks[1] / total_marks) * 100).toFixed(2) + "%\n" +
+//     "Score of " + st[2] + " is " + ((marks[2] / total_marks) * 100).toFixed(2) + "%\n";
+
+// alert(result);
+
+
+// <!-- ---answer 9 --- -->
+
